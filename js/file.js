8a541eb7f7d12794f4b8ae47ea3a910406b19e1e
mohamed <33775327+benessaessa@users.noreply.github.com>
2019-09-23 12:39:26 +0200

    update

--- js/file.js
+++ js/file.js
@@ -13,3 +13,18 @@ function togglePasswordFieldClicked() {
 	passwordField.value = value;
 
 }
+function togglePassword() {
+
+	var passwordField = document.getElementById('passwordField2');
+	var value = passwordField.value;
+
+	if(passwordField.type == 'password') {
+        passwordField.type = 'text';
+	}
+	else {
+		passwordField.type = 'password';
+	}
+	
+	passwordField.value = value;
+
+}
